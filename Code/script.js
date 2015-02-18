688a8db8f5b21730a1f9ba6d8ea1e13fbb2a2e84
Wiebke Koepp <wiebke.koepp@googlemail.com>
2015-02-18 16:07:12 +0100

    Added Class for Integers Adjoined Square Root of 2

--- Code/script.js
+++ Code/script.js
@@ -1,3 +1,39 @@
 /**
- * Created by Wiebke on 15.02.15.
+ * Class for numbers in the ring of integers adjoined square root of 2
  */
+var SQRT2 = Math.sqrt(2);
+
+/* Constructor */
+function IntAdjoinSqrt2(coeffInt, coeffSqrt){
+    this.coeffInt = coeffInt;
+    this.coeffSqrt = coeffSqrt;
+};
+
+/* Getter methods */
+IntAdjoinSqrt2.prototype.coeffInt = function(){return this.coeffInt};
+IntAdjoinSqrt2.prototype.coeffSqrt = function(){return this.coeffSqrt};
+
+/* Conversion */
+IntAdjoinSqrt2.prototype.toFloat = function(){
+    return this.coeffInt + this.coeffSqrt*SQRT2;
+};
+
+ /* Basic arithmetic - Adding another number to this one */
+IntAdjoinSqrt2.prototype.add =function(other){
+    this.coeffInt += other.coeffInt;
+    this.coeffSqrt += other.coeffSqrt;
+};
+
+ /* Basic arithmetic - Subtracting another number from this one */
+IntAdjoinSqrt2.prototype.subtract = function(other){
+    this.coeffInt -= other.coeffInt;
+    this.coeffSqrt -= other.coeffSqrt;
+};
+
+ /* Basic arithmetic - Multiplying this number by another one */
+IntAdjoinSqrt2.prototype.multiply = function(other){
+    /* (a + bx)*(c+dx) = (a*c + b*d*x*x) + (a*d + b*c)  */
+    var coeffIntCopy = this.coeffInt;
+    this.coeffInt = coeffIntCopy*other.coeffInt + 2*this.coeffSqrt*other.coeffSqrt;
+    this.coeffSqrt = coeffIntCopy*other.coeffSqrt + this.coeffSqrt*other.coeffInt;
+};
