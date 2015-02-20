5275ce5a5392a57a313bf01f5f4c7034ec9edd50
Wiebke Koepp <wiebke.koepp@googlemail.com>
2015-02-20 12:21:38 +0100

    Drawing of Tangram with Correct Orientations

--- Code/script.js
+++ Code/script.js
@@ -26,12 +26,14 @@ IntAdjoinSqrt2.prototype.toFloat = function(){
 IntAdjoinSqrt2.prototype.add =function(other){
     this.coeffInt += other.coeffInt;
     this.coeffSqrt += other.coeffSqrt;
+    return this;
 };
 
  /* Basic arithmetic - Subtracting another number from this one */
 IntAdjoinSqrt2.prototype.subtract = function(other){
     this.coeffInt -= other.coeffInt;
     this.coeffSqrt -= other.coeffSqrt;
+    return this;
 };
 
  /* Basic arithmetic - Multiplying this number by another one */
@@ -40,6 +42,14 @@ IntAdjoinSqrt2.prototype.multiply = function(other){
     var coeffIntCopy = this.coeffInt;
     this.coeffInt = coeffIntCopy*other.coeffInt + 2*this.coeffSqrt*other.coeffSqrt;
     this.coeffSqrt = coeffIntCopy*other.coeffSqrt + this.coeffSqrt*other.coeffInt;
+    return this;
+};
+
+/* Basic arithmetic - Negation */
+IntAdjoinSqrt2.prototype.neg = function(){
+    this.coeffInt = -this.coeffInt;
+    this.coeffSqrt = -this.coeffSqrt;
+    return this;
 };
 
 /*
@@ -75,13 +85,14 @@ Point.prototype.z = function(){return this.z};
 /* Duplication */
 Point.prototype.dup = function(){
     return new Point(this.x.dup(), this.y.dup(), this.z.dup());
-}
+};
 
 /* Combinations of Points */
 Point.prototype.add = function(other){
     this.x.add(other.x);
     this.y.add(other.y);
     this.z.add(other.z);
+    return this;
 };
 
 Point.prototype.middle = function(other){
@@ -89,21 +100,71 @@ Point.prototype.middle = function(other){
 };
 
 Point.prototype.subtract = function(other){
-    this.x -= other.x;
-    this.y -= other.y;
-    this.z -= other.z;
+    this.x.subtract(other.x);
+    this.y.subtract(other.y);
+    this.z.subtract(other.z);
+    return this;
 };
 
+/* Transform a point by a given 3x3-rotation matrix */
 Point.prototype.transform = function(transMatrix){
-
+    if(transMatrix.length != 3){
+        console.log("Matrix seems to have the wrong dimension!");
+        return;
+    }
+    var copy = this.dup();
+    this.x = copy.x.dup().multiply(transMatrix[0][0]);
+    this.x.add(copy.y.dup().multiply(transMatrix[0][1]));
+    this.x.add(copy.z.dup().multiply(transMatrix[0][2]));
+    this.y = copy.x.dup().multiply(transMatrix[1][0]);
+    this.y.add(copy.y.dup().multiply(transMatrix[1][1]));
+    this.y.add(copy.z.dup().multiply(transMatrix[1][2]));
+    this.z = copy.x.dup().multiply(transMatrix[2][0]);
+    this.z.add(copy.y.dup().multiply(transMatrix[2][1]));
+    this.z.add(copy.z.dup().multiply(transMatrix[2][2]));
+    return this;
 };
 
 Point.prototype.translate = function(transX, transY){
-
+    var translationMatrix =
+        [[new IntAdjoinSqrt2(1,0), new IntAdjoinSqrt2(0,0), transX],
+            [new IntAdjoinSqrt2(0,0), new IntAdjoinSqrt2(1,0), transY],
+            [new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(1,0)]];
+    this.transform(translationMatrix);
 };
 
-Point.prototype.rotate = function(){
+Point.prototype.rotate = function(angle){
+    // Transform angle to that it falls in the interval [0;360]
 
+    // If angle is not a multiple of 45 degrees, we will not use it for rotation
+    if (angle % 45 != 0){
+        console.log("Rotations around " + angle + "degrees are not supported!");
+        return;
+    }
+    var cos;
+    var sin;
+    // Determine value of sin and cos
+    if (angle % 90 != 0){
+        cos = new IntAdjoinSqrt2(0,0.5);
+        sin = new IntAdjoinSqrt2(0,0.5);
+    } else if (angle % 180 != 0) {
+        cos = new IntAdjoinSqrt2(0,0);
+        sin = new IntAdjoinSqrt2(1,0);
+    } else {
+        cos = new IntAdjoinSqrt2(1,0);
+        sin = new IntAdjoinSqrt2(0,0);
+    }
+    // Determine the sign of sin and cos
+    if (angle > 180 && angle <360){
+        sin.neg();
+    }
+    if (angle >90 && angle < 270){
+        cos.neg();
+    }
+    var rotationMatrix = [[cos, sin.dup().neg(), new IntAdjoinSqrt2(0,0)],
+        [sin, cos,new IntAdjoinSqrt2(0,0)],
+        [new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(1,0)]];
+    this.transform(rotationMatrix);
 };
 
 Point.prototype.toFloatX = function(){
@@ -118,19 +179,15 @@ Point.prototype.toFloatY = function(){
  * Class for a Tan
  */
 
-/* Constructor: type is a number from 0 to 4, depending on which tan the object
-* describes, anchor is one specific point, orientation is a number between
-* 0 (0 degrees) and 7 (315 degrees) and flipped is a boolean, which is only
-* relevant for the parallelogram */
-function Tan(tanType,anchor,orientation,flipped) {
+/* Constructor: tanType is a number from 0 to 5, depending on which tan the object
+* describes where 0: big triangle, 1: medium triangle, 2: small triangle,
+* 3: square, 4: parallelogram and 5: flipped parallelogram, anchor is one specific
+* point, orientation is a number between
+* 0 (0 degrees) and 7 (315 degrees) */
+function Tan(tanType,anchor,orientation) {
     this.tanType = tanType;
     this.anchor = anchor;
     this.orientation = orientation;
-    if (flipped === 'undefined') {
-        this.flipped = false;
-    } else {
-        this.flipped = flipped;
-    }
 }
 
 /**
@@ -144,57 +201,60 @@ function Tan(tanType,anchor,orientation,flipped) {
  * | /    |    |    \     \    /     /
  * b      c----b     c-----b  x-----a
  */
-var Directions = {};
-Directions.BigTriangle =
-    [new Point(new IntAdjoinSqrt2(0,2),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,2),new IntAdjoinSqrt2(0,0))];
-Directions.MediumTriangle =
-    [new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0))];
-Directions.SmallTriangle =
-    [new Point(new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0))];
-Directions.Square =
-    [new Point(new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0))];
-Directions.Parallelogram =
-    [new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(3,0),new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(0,0))];
-Directions.ParallelogramFlipped =
-    [new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(3,0),new IntAdjoinSqrt2(-1,0),new IntAdjoinSqrt2(0,0)),
-        new Point(new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(-1,0),new IntAdjoinSqrt2(0,0))];
+var Directions = [];
+
+var fillDirections = function(){
+    // Create Array for each tan piece
+    for (var i = 0; i <= 5; i++){
+        Directions[i] = [];
+    }
+    // Fill the first entry of each array with the direction vectors for when
+    // a piece is not rotated
+    Directions[0][0] =
+        [new Point(new IntAdjoinSqrt2(0,2),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,2),new IntAdjoinSqrt2(0,0))];
+    Directions[1][0] =
+        [new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0))];
+    Directions[2][0] =
+        [new Point(new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0))];
+    Directions[3][0] =
+        [new Point(new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,1),new IntAdjoinSqrt2(0,0))];
+    Directions[4][0] =
+        [new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(3,0),new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(0,0))];
+    Directions[5][0] =
+        [new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(3,0),new IntAdjoinSqrt2(-1,0),new IntAdjoinSqrt2(0,0)),
+            new Point(new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(-1,0),new IntAdjoinSqrt2(0,0))];
+    // Matrix for rotating by 45 degrees
+    var rotationMatrix =
+        [[new IntAdjoinSqrt2(0,0.5), new IntAdjoinSqrt2(0,-0.5), new IntAdjoinSqrt2(0,0)],
+            [new IntAdjoinSqrt2(0,0.5), new IntAdjoinSqrt2(0,0.5), new IntAdjoinSqrt2(0,0)],
+            [new IntAdjoinSqrt2(0,0), new IntAdjoinSqrt2(0,0), new IntAdjoinSqrt2(1,0)]];
+    // For each type, rotate the direction vectors of orientation orientID by 45
+    // degrees to get the direction vectors for orientation orientID+1
+    for(var tanTypeID = 0; tanTypeID <= 5; tanTypeID++){
+        for(var orientID = 0; orientID < 7; orientID++){
+            Directions[tanTypeID][orientID+1] = [];
+            for(var dir = 0; dir < Directions[tanTypeID][0].length; dir++){
+                console.log(tanTypeID + " " + orientID + " " + dir);
+                Directions[tanTypeID][orientID+1][dir] =
+                    Directions[tanTypeID][orientID][dir].dup().transform(rotationMatrix);
+            }
+        }
+    }
+    console.log(JSON.stringify(Directions));
+};
 
 Tan.prototype.toSVG = function(){
     var points = "" + this.anchor.toFloatX() + "," + this.anchor.toFloatY() + " ";
-    var directions;
-    switch (this.tanType){
-        case 0:
-            directions = Directions.BigTriangle;
-            break;
-        case 1:
-            directions = Directions.MediumTriangle;
-            break;
-        case 2:
-            directions = Directions.SmallTriangle;
-            break;
-        case 3:
-            directions = Directions.Square;
-            break;
-        case 4:
-            if (this.flipped){
-                directions = Directions.ParallelogramFlipped;
-            } else {
-                directions = Directions.Parallelogram
-            }
-            break;
-        default:
-            directions = [];
-    }
+    var directions = Directions[this.tanType][this.orientation];
     for (var i = 0; i < directions.length; i++){
-        console.log(i);
         var current = this.anchor.dup();
         current.add(directions[i]);
         points += current.toFloatX() + "," + current.toFloatY() + " ";
@@ -215,6 +275,8 @@ Tangram.prototype.toSVG = function(){
     for(var i = 0; i < this.tans.length; i++){
         var shape = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
         shape.setAttribute("points", this.tans[i].toSVG());
+        // Fill with random color for now
+        shape.setAttribute("fill", '#'+Math.random().toString(16).substr(-6));
         tangramSVG.appendChild(shape);
     }
     return tangramSVG;
@@ -222,23 +284,24 @@ Tangram.prototype.toSVG = function(){
 
 /* Test tangram */
 var anchor1 = new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(1,0));
-var bigTriangle1 = new Tan(0, anchor1, 7);
+var bigTriangle1 = new Tan(0, anchor1, 1);
 var anchor2 = new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(1,0));
-var bigTriangle2 = new Tan(0, anchor2, 1);
+var bigTriangle2 = new Tan(0, anchor2, 7);
 var anchor3 = new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(1,0));
 var mediumTriangle = new Tan(1, anchor3, 0);
 var anchor4 = new Point(new IntAdjoinSqrt2(1,0),new IntAdjoinSqrt2(3,0),new IntAdjoinSqrt2(1,0));
-var smallTriangle1 = new Tan(2, anchor4, 6);
+var smallTriangle1 = new Tan(2, anchor4, 3);
 var anchor5 = new Point(new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(1,0));
-var smallTriangle2 = new Tan(2, anchor5, 3);
+var smallTriangle2 = new Tan(2, anchor5, 5);
 var anchor6 = new Point(new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(2,0),new IntAdjoinSqrt2(1,0));
-var square = new Tan(3, anchor6, 1);
+var square = new Tan(3, anchor6, 7);
 var anchor7 = new Point(new IntAdjoinSqrt2(4,0),new IntAdjoinSqrt2(0,0),new IntAdjoinSqrt2(1,0));
-var parallelogram = new Tan(4, anchor7, 4);
+var parallelogram = new Tan(5, anchor7, 4);
 
 var test = new Tangram([bigTriangle1,bigTriangle2,mediumTriangle,smallTriangle1,smallTriangle2,square,parallelogram]);
 
 window.onload = function(){
+    fillDirections();
     var svgElement = test.toSVG();
     document.getElementById("content").appendChild(svgElement);
 };
